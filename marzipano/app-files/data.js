7880338f36879e18c1aae2455e8b1b6d89019af9
mati <mrosini@isep-cba.edu.ar>
2024-08-13 11:46:35 -0300

    marzipano

---
+++ marzipano/app-files/data.js
@@ -0,0 +1,170 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-street-view-360",
+      "name": "Street View 360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 416,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-street-view-3602",
+      "name": "Street View 3602",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 416,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.2706431484072205,
+          "pitch": -0.4806858290241962,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">La fundición de las armas para el Ejército de los Andes</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">La fundición de las armas para el Ejército de los Andes</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "2-street-view-3603",
+      "name": "Street View 3603",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 416,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-street-view-3604",
+      "name": "Street View 3604",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 416,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-street-view-3605",
+      "name": "Street View 3605",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 416,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.113863045265475,
+          "pitch": -0.14531501750677833,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">vvv</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Encuentro del Ejército de los Andes con los baqueanos</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "5-street-view-3606-",
+      "name": "Street View 3606-",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 416,
+      "initialViewParameters": {
+        "yaw": -1.3653844994448292,
+        "pitch": -0.44155361118881586,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.1929368397227584,
+          "pitch": -0.46503578319689964,
+          "title": "<div><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Damas de la alta sociedad entregando sus joyas para colaborar con la campaña</font></font></div>",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Cerro de la Gloria",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
