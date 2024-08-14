67dc2372c87f399fa3df4a27ad8e6539662bc63d
mati <mrosini@isep-cba.edu.ar>
2024-08-14 11:51:30 -0300

    marzipano2

---
+++ marzipano/monumento-de-la-gloria/app-files/data.js
@@ -0,0 +1,243 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-primera-vista",
+      "name": "primera-vista",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 0.24031841238999796,
+        "pitch": -0.4155798693541364,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5132134314480652,
+          "pitch": -0.22965989185028235,
+          "rotation": 0,
+          "target": "1-emcuentro"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.012691173299181457,
+          "pitch": -0.5364481055144612,
+          "title": "Escudo argentino",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.2989210059932521,
+          "pitch": -0.3666073808818435,
+          "title": "<div>Granaderos escoltando a San Martín desde los flancos</div><div><br></div>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.12070799895350781,
+          "pitch": -0.7821133187601745,
+          "title": "La libertad, rompiendo sus cadenas",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.07222151171535351,
+          "pitch": -0.3504500119687144,
+          "title": "San Martín en su caballo. Su posición indica que no ha muerto en batalla",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-emcuentro",
+      "name": "emcuentro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4500245008489916,
+          "pitch": -0.1401089982795618,
+          "rotation": 0,
+          "target": "2-damas"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9688452286759066,
+          "pitch": -0.45651880571096015,
+          "title": "Encuentro del Ejército de los Andes con los baqueanos&nbsp;",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.7259967199980775,
+          "pitch": -0.5834227198379587,
+          "title": "Escudo chileno",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.7503991586558243,
+          "pitch": -0.9682499640136655,
+          "title": "Halcón levantando vuelo, símbolo de la gran hazaña&nbsp;",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-damas",
+      "name": "damas",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1518192490809511,
+          "pitch": -0.07414941295858846,
+          "rotation": 0,
+          "target": "3-armas"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4213408628080533,
+          "pitch": -0.19218970344022956,
+          "title": "Damas de la alta sociedad entregando sus joyas para colaborar con la campaña",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-armas",
+      "name": "armas",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8664490022866449,
+          "pitch": -0.08292293952183982,
+          "rotation": 0,
+          "target": "0-primera-vista"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5856557852419257,
+          "pitch": -0.08006045421542751,
+          "title": "La fundición de las armas para el Ejército de los Andes",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.5919956208901045,
+          "pitch": -0.3538779742375997,
+          "title": "Escudo peruano",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Monumento de la Gloria",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
